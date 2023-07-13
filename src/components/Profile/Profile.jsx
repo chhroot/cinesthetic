b3bbe12736aecbdd0f87b09baa993c8f9c7752c6
Amruth Kiran <amruthkiran47@gmail.com>
2023-07-13 21:56:58 +0530

    ft: profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Box, Typography, Button } from '@mui/material';
+import { ExitToApp } from '@mui/icons-material';
+
+import { userSelector } from '../../features/auth';
 
 function Profile() {
-  console.log('g');
+  const { user } = useSelector(userSelector);
+
+  const favoriteMovies = [];
+
+  const logout = () => {
+    localStorage.clear();
+
+    window.location.href = '/';
+  };
 
   return (
-    <div>Profile</div>
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>My Profile</Typography>
+        <Button color="inherit" onclick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favoriteMovies.length
+        ? <Typography variant="5">Add Favorites or watchlist some movies to see them here!</Typography>
+        : (
+          <Box>
+            FAVOURITE MOVIES
+          </Box>
+        )}
+
+    </Box>
   );
 }
 
