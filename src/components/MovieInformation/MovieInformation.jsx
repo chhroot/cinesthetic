e5a27404426ebb536139d36a491ab92c94551d29
Amruth Kiran <amruthkiran47@gmail.com>
2023-07-14 21:25:22 +0530

    ft: movie info

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Modal, Typography, Button, ButtonGroup, Grid, Box, CircularProgress, useMediaQuery, Rating } from '@mui/material';
+import { Movie as MovieIcon, Theaters, Language, PlusOne, Favorite, FavoriteBorderOutlined, Remove, ArrowBack } from '@mui/icons-material';
+import { Link, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+
+import { useGetMovieQuery } from '../../services/TMDB';
 
 function MovieInformation() {
-  console.log('g');
+  const { id } = useParams();
+  const { data, isFetching, error } = useGetMovieQuery(id);
 
   return (
     <div>MovieInformation</div>
