83b57dd27b5a787a95bb54828c077c6ca6e6e0db
Amruth Kiran <amruthkiran47@gmail.com>
2023-07-18 12:43:47 +0530

    ft: movie trailer

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Modal, Typography, Button, ButtonGroup, Grid, Box, CircularProgress, useMediaQuery, Rating,
 } from '@mui/material';
 import {
-  Movie as MovieIcon, Theaters, Language, PlusOne, Favorite, FavoriteBorderOutlined, Remove, ArrowBack,
+  Movie as MovieIcon,
+  Theaters,
+  Language, PlusOne, Favorite, FavoriteBorderOutlined, Remove, ArrowBack,
 } from '@mui/icons-material';
 import { Link, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
@@ -20,6 +22,7 @@ function MovieInformation() {
   const { data, isFetching, error } = useGetMovieQuery(id);
   const classes = useStyles();
   const dispatch = useDispatch();
+  const [open, setopen] = useState(false);
 
   const { data: recommendations, isFetching: isRecommendationsFetching } = useGetRecommendationsQuery({ list: '/recommendations', movie_id: id });
 
@@ -113,7 +116,7 @@ function MovieInformation() {
                 <ButtonGroup size="medium" variant="outlined">
                   <Button target="_blank" rel="noopener noreferrer" href={data?.homepage} endIcon={<Language />}>Website</Button>
                   <Button target="_blank" rel="noopener noreferrer" href={`https://www.imdb.com/title/${data?.imdb_id}`} endIcon={<MovieIcon />}>IMDB</Button>
-                  <Button onClick={() => {}} href="#" endIcon={<Theaters />}>Trailer</Button>
+                  <Button onClick={() => setopen(true)} href="#" endIcon={<Theaters />}>Trailer</Button>
                 </ButtonGroup>
               </Grid>
               <Grid item xs={12} sm={6} className={classes.buttonContainer}>
@@ -149,6 +152,23 @@ function MovieInformation() {
           ? <MovieList movies={recommendations} numberOfMovies={12} />
           : <Box>Sorry, nothing was found</Box>}
       </Box>
+      <Modal
+        closeAfterTransition
+        className={classes.modal}
+        open={open}
+        onClose={() => setopen(false)}
+      >
+        {data?.videos?.results?.length > 0 && (
+          <iframe
+            autoPlay
+            className={classes.video}
+            frameBorder="0"
+            title="Trailer"
+            src={`https://www.youtube.com/embed/${data.videos.results[0].key}`}
+            allow="autoplay"
+          />
+        )}
+      </Modal>
     </Grid>
   );
 }
