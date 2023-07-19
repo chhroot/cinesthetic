dce18904fab6cf5f32c37450a580ec9fd9a56b7b
Amruth Kiran <amruthkiran47@gmail.com>
2023-07-19 14:00:46 +0530

    ft: featured card

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -10,6 +10,7 @@ import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
 import { useGetMoviesQuery } from '../../services/TMDB';
 import MovieList from '../MovieList/MovieList';
 import Pagination from '../Pagination/Pagination';
+import FeaturedMovie from '../FeaturedMovie/FeaturedMovie';
 
 function Movies() {
   const [page, setPage] = useState(1);
@@ -24,7 +25,7 @@ function Movies() {
   } = useGetMoviesQuery({ genreIdOrCategoryName, page, searchQuery });
   const lg = useMediaQuery((theme) => theme.breakpoints.only('lg'));
 
-  const numberOfMovies = lg ? 16 : 18;
+  const numberOfMovies = lg ? 17 : 19;
 
   if (isFetching) {
     return (
@@ -45,7 +46,8 @@ function Movies() {
   if (error) return 'An error occured';
   return (
     <div>
-      <MovieList movies={data} numberOfMovies={numberOfMovies} />
+      <FeaturedMovie movie={data.results[0]} />
+      <MovieList movies={data} numberOfMovies={numberOfMovies} excludeFirst />
       <Pagination currentPage={page} setPage={setPage} totalPages={data?.total_pages} />
     </div>
   );
